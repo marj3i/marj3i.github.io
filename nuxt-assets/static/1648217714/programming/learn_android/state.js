160eb21bbc12e4a5e45acacaeb5eadb04ba5772a
fares <djefaflia.fares@gmail.com>
2022-03-25 15:15:21 +0100

    Updates

---
+++ nuxt-assets/static/1648217714/programming/learn_android/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-assets\u002Fstatic\u002F1648217714",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprogramming\u002Flearn_android",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-assets\u002F",cdnURL:a},content:{dbHash:"c64e24a0"}}}}(null));
---
+++ nuxt-assets/static/1648217714/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt-assets\u002Fstatic\u002F1648217714",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fnuxt-assets\u002F",cdnURL:a},content:{dbHash:"c64e24a0"}}}}(null,"\u002F"));
