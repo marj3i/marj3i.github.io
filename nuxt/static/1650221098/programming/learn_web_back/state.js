5bb60be7944879cd312d9e2f6aa62f3e38ecc47a
fares <djefaflia.fares@gmail.com>
2022-04-17 19:45:02 +0100

    Updates

---
+++ nuxt/static/1650221098/programming/learn_web_back/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1650221098",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprogramming\u002Flearn_web_back",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"1b992844"}}}}(null));
