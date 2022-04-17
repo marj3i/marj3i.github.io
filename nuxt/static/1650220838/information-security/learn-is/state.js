4e741e1f8349c162c069bf7661dc5afaf5b2bff5
fares <djefaflia.fares@gmail.com>
2022-04-17 19:40:40 +0100

    Updates

---
+++ nuxt/static/1650220838/information-security/learn-is/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1650220838",layout:"default",error:a,serverRendered:true,routePath:"\u002Finformation-security\u002Flearn-is",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"75d4fdef"}}}}(null));
---
+++ nuxt/static/1650220838/programming/tracks/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1650220838",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprogramming\u002Ftracks",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"75d4fdef"}}}}(null));
