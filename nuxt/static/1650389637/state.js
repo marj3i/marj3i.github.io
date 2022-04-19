6e407fd9c7b46865555764dfc3da906c984364e5
fares <djefaflia.fares@gmail.com>
2022-04-19 18:34:00 +0100

    Updates

---
+++ nuxt/static/1650389637/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1650389637",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"eb1bbbdf"}}}}(null,"\u002F"));
